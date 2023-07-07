da95b1505da5fd537f640596b804a73b633e9daf
Hendro Steven <hendro.steven@gmail.com>
2023-07-07 23:13:26 +0700

    update config db to heroku env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,15 @@
+// module.exports = {
+//   HOST: "localhost",
+//   PORT: "3306",
+//   USER: "root",
+//   PASSWORD: "admin",
+//   DB: "dbamazon",
+// };
+
 module.exports = {
-  HOST: "localhost",
+  HOST: "acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   PORT: "3306",
-  USER: "root",
-  PASSWORD: "admin",
-  DB: "dbamazon",
+  USER: "o09jm5z879yc2o5z",
+  PASSWORD: "kjewfl4uewikpyof",
+  DB: "jn3waw10pso9ld35",
 };
