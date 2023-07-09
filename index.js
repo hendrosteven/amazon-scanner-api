33abb40527607e3525bae2f5e2a4608f805682d3
Hendro Steven <hendro.steven@gmail.com>
2023-07-09 22:04:41 +0700

    enable cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(cors());
 require("dotenv").config({ path: __dirname + "/.env" });
 
 app.use(bodyParser.json());
